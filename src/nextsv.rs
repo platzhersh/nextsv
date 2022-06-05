9ea437b164b75308c9ae24cb1951f0be82468105
Jeremiah Russell <jrussell@jerus.ie>
2022-06-05 10:04:05 +0100

    feat(Semantic Version): ✨ add parse method to parse a git tag into a semantic version
    parse method returns a result of either the Semantic struct  or the error
    detected.
    Add error module to provide errors detected during the parse
    attempt.
    Derive functions required to enable a vector Semantic values to be
    sorted.

--- src/nextsv.rs
+++ src/nextsv.rs
@@ -13,9 +13,11 @@
 
 use std::fmt;
 
+mod error;
+use error::Error;
 /// The Semantic data structure represents a semantic version number.
-#[derive(Debug, Default)]
-struct Semantic {
+#[derive(Debug, Default, PartialEq, PartialOrd, Eq, Ord)]
+pub struct Semantic {
     major: usize,
     minor: usize,
     patch: usize,
@@ -28,12 +30,42 @@ impl fmt::Display for Semantic {
 }
 
 impl Semantic {
+    // Create a new struct specifying each of the semantic version components.
     fn new(major: usize, minor: usize, patch: usize) -> Self {
         Semantic {
             major,
             minor,
             patch,
         }
+    }
+    /// Parse a tag and return a struct
+    /// String format expect: vx.y.z
+    pub fn parse(tag: &str) -> Result<Self, Error> {
+        if !tag.starts_with('v') {
+            return Err(Error::NotVersionTag(tag.to_string()));
+        }
+        let version = tag[1..].to_string();
+        let components: Vec<&str> = version.split('.').collect();
+
+        let mut count_numbers = 0;
+        let mut numbers = vec![];
+
+        for item in components {
+            count_numbers += 1;
+            if count_numbers > 3 {
+                return Err(Error::TooManyComponents(count_numbers));
+            }
+            numbers.push(match item.parse::<usize>() {
+                Ok(n) => n,
+                Err(_) => return Err(Error::MustBeNumber(item.to_string())),
+            });
+        }
+
+        if count_numbers < 3 {
+            return Err(Error::NotEnoughComponents(count_numbers));
+        }
+
+        Ok(Semantic::new(numbers[0], numbers[1], numbers[2]))
     }
     /// Increment the patch component of the version number by 1
     fn increment_patch(self) -> Self {
@@ -58,6 +90,7 @@ impl Semantic {
 #[cfg(test)]
 mod tests {
     use crate::nextsv::*;
+    use claim::*;
 
     #[test]
     fn display_semantic_version_number() {
@@ -89,4 +122,80 @@ mod tests {
 
         assert_eq!("1.0.0", &updated_version.to_string());
     }
+
+    #[test]
+    fn parse_valid_version_tag_to_new_semantic_struct() {
+        let tag = "v0.3.90";
+        let semantic = Semantic::parse(tag);
+
+        assert_ok!(&semantic);
+        let semantic = match semantic {
+            Ok(s) => s.to_string(),
+            Err(e) => e.to_string(),
+        };
+        assert_eq!(tag[1..], semantic);
+    }
+
+    #[test]
+    fn parse_error_failed_not_version_tag() {
+        let tag = "0.3.90";
+        let semantic = Semantic::parse(tag);
+
+        assert_err!(&semantic);
+        let semantic = match semantic {
+            Ok(s) => s.to_string(),
+            Err(e) => e.to_string(),
+        };
+        assert_eq!(
+            "Version tags must start with the letter 'v' but tag is 0.3.90",
+            semantic
+        );
+    }
+
+    #[test]
+    fn parse_error_too_many_components() {
+        let tag = "v0.3.90.8";
+        let semantic = Semantic::parse(tag);
+
+        assert_err!(&semantic);
+        let semantic = match semantic {
+            Ok(s) => s.to_string(),
+            Err(e) => e.to_string(),
+        };
+        assert_eq!(
+            "Version must have three components but at least 4 were found",
+            semantic
+        );
+    }
+
+    #[test]
+    fn parse_error_not_enough_components() {
+        let tag = "v0.3";
+        let semantic = Semantic::parse(tag);
+
+        assert_err!(&semantic);
+        let semantic = match semantic {
+            Ok(s) => s.to_string(),
+            Err(e) => e.to_string(),
+        };
+        assert_eq!(
+            "Version must have three components but only 2 found",
+            semantic
+        );
+    }
+
+    #[test]
+    fn parse_error_version_must_be_a_number() {
+        let tag = "v0.3.90-8";
+        let semantic = Semantic::parse(tag);
+
+        assert_err!(&semantic);
+        let semantic = match semantic {
+            Ok(s) => s.to_string(),
+            Err(e) => e.to_string(),
+        };
+        assert_eq!("Version must be a number but found 90-8", semantic);
+    }
+    // #[error("Version must be a number")]
+    // MustBeNumber,
 }

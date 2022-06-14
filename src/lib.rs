94363cb6c5923c087d6ee5e77596174d9fe17263
Jeremiah Russell <jrussell@jerus.ie>
2022-06-14 08:20:12 +0100

    feat(Semantic Version): Count conventional commits to last tag

--- src/lib.rs
+++ src/lib.rs
@@ -14,28 +14,78 @@
 mod error;
 mod semantic;
 
-use error::Error;
+pub use error::Error;
 use semantic::Semantic;
 
-use git2::Repository;
+use git2::{Oid, Repository, Sort};
+
+#[derive(Debug, PartialEq, PartialOrd, Eq, Clone, Ord)]
+pub struct Tag {
+    name: Semantic,
+    id: Oid,
+}
+
+impl Tag {
+    fn new(name: Semantic, id: Oid) -> Self {
+        Tag { name, id }
+    }
+    pub fn name(&self) -> Semantic {
+        self.name
+    }
+    pub fn id(&self) -> Oid {
+        self.id
+    }
+}
 
 /// Get a list of the version tags in the repo and identify the latest version
 ///
-pub fn get_latest_version_tag() -> Result<Semantic, Error> {
+pub fn latest_version_tag() -> Result<Tag, Error> {
     let repo = Repository::open(".")?;
-
-    let tags = repo.tag_names(Some("v*")).unwrap();
-
     let mut versions = vec![];
+    repo.tag_foreach(|id, name| {
+        if let Ok(name) = String::from_utf8(name.to_owned()) {
+            if let Some(name) = name.strip_prefix("refs/tags/") {
+                println!("id: {:?} name: {:?}", id, name);
+                if name.starts_with('v') {
+                    if let Ok(semantic_version) = Semantic::parse(name) {
+                        versions.push(Tag::new(semantic_version, id));
+                    }
+                }
+            }
+        }
+        true
+    })?;
 
-    for tag in tags.iter().flatten() {
-        if let Ok(s) = Semantic::parse(tag) {
-            versions.push(s)
-        };
-    }
     versions.sort();
-    match versions.last() {
-        Some(last_version) => Ok(last_version.to_owned()),
+    let last_version = versions.last().cloned();
+
+    match last_version {
+        Some(v) => Ok(v),
         None => Err(Error::NoVersionTag),
     }
+}
+
+pub fn conventional_commits_to_tag(last_release_tag: Semantic) -> Result<Vec<String>, Error> {
+    let repo = Repository::open(".")?;
+
+    let mut walk = repo.revwalk()?;
+    walk.set_sorting(Sort::REVERSE)?;
+
+    let mut ret = vec![];
+
+    for c in walk.into_iter().flatten() {
+        let commit = repo.find_commit(c)?;
+        let message = commit.message();
+        if let Some(m) = message {
+            ret.push(m.to_string())
+        }
+
+        if let Ok(tag) = repo.find_tag(c) {
+            if tag.name() == Some(&last_release_tag.to_string()) {
+                break;
+            }
+        }
+    }
+
+    Ok(ret)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,100 @@
-fn main() {
+use nextsv_lib::Error;
+
+fn main() -> Result<(), Error> {
     println!("Hello World");
-    match nextsv_lib::get_latest_version_tag() {
-        Ok(version) => println!("Latest version is: {}", version),
-        Err(e) => println!("Error: {}", e),
-    };
+
+    // What is the latest version tag?
+    let latest_version = nextsv_lib::latest_version_tag()?;
+    println!("Latest version is: {:?}", &latest_version);
+
+    // What are the conventional commits since that tag?
+    let conventional_commits = nextsv_lib::conventional_commits_to_tag(latest_version.name())?;
+    println!("Conventional Commits: {:?}", conventional_commits);
+
+    let repo = git2::Repository::open(".")?;
+    let mut revwalk = repo.revwalk()?;
+    revwalk.set_sorting(git2::Sort::NONE)?;
+    revwalk.push_head()?;
+
+    macro_rules! filter_try {
+        ($e:expr) => {
+            match $e {
+                Ok(t) => t,
+                Err(e) => return Some(Err(e)),
+            }
+        };
+    }
+
+    #[allow(clippy::unnecessary_filter_map)]
+    let revwalk = revwalk.filter_map(|id| {
+        let id = filter_try!(id);
+        let commit = repo.find_commit(id);
+        let commit = filter_try!(commit);
+        Some(Ok(commit))
+    });
+
+    // let mut counter = 0;
+    let mut count_feature = 0;
+    let mut count_fix = 0;
+    let mut count_docs = 0;
+    let mut count_chore = 0;
+    let mut count_refactor = 0;
+    let mut breaking = false;
+
+    for commit in revwalk {
+        let commit = commit?;
+        // counter += 1;
+        // Break once we find the latest version tag
+        if commit.id() == latest_version.id() {
+            break;
+        }
+
+        if let Ok(conventional) =
+            git_conventional::Commit::parse(commit.summary().take().unwrap_or("default"))
+        {
+            if conventional.type_() == git_conventional::Type::FEAT {
+                count_feature += 1;
+            }
+
+            if conventional.type_() == git_conventional::Type::FIX {
+                count_fix += 1;
+            }
+
+            if conventional.type_() == git_conventional::Type::DOCS {
+                count_docs += 1;
+            }
+
+            if conventional.type_() == git_conventional::Type::CHORE {
+                count_chore += 1;
+            }
+
+            if conventional.type_() == git_conventional::Type::REFACTOR {
+                count_refactor += 1;
+            }
+            if !breaking {
+                breaking = conventional.breaking();
+            }
+
+            // if let Some(summary) = &commit.summary() {
+            //     println!(
+            //         "{}: {} {}",
+            //         counter,
+            //         commit.id().to_string().get(..6).unwrap_or(""),
+            //         summary
+            //     );
+            // }
+        }
+    }
+    println!("Conventional commits by type:");
+    println!("  feat:       {}", count_feature);
+    println!("  fix:        {}", count_fix);
+    println!("  docs:       {}", &count_docs);
+    println!("  chore:      {}", count_chore);
+    println!("  refactor:   {}", count_refactor);
+    if breaking {
+        println!("One or more breaking changes")
+    } else {
+        println!("No breaking change.")
+    }
+    Ok(())
 }

5d3a412eeed59b7ad40d9a6938d6e087ee3f876b
Jeremiah Russell <jrussell@jerus.ie>
2022-06-26 09:06:27 +0100

    chore: 📝 Update release version in Cargo.toml to 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
+use clap::Parser;
 use nextsv_lib::Error;
 use nextsv_lib::VersionTag;
 
-fn main() -> Result<(), Error> {
-    println!("Next Version\n------------\n");
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct NextsvCliArgs {
+    // #[clap(short, long, value_parser)]
+    // name: String,
+    // #[clap(short, long, value_parser, default_value_t = 1)]
+    // count: u8,
+    #[clap(short, long, value_parser, default_value_t = false)]
+    verbose: bool,
+}
 
+fn main() -> Result<(), Error> {
     // What is the latest tag?
     // What are the conventional commits since that tag?
+    let args = NextsvCliArgs::parse();
+
     let latest_version = VersionTag::latest("v")?.commits()?;
-
-    println!(
-        "Conventional commits by type for version: {}",
-        latest_version.name()
-    );
-    println!("  feat:       {}", latest_version.feat_commits());
-    println!("  fix:        {}", latest_version.fix_commits());
-    println!("  docs:       {}", latest_version.docs_commits());
-    println!("  chore:      {}", latest_version.chore_commits());
-    println!("  refactor:   {}", latest_version.refactor_commits());
-    if latest_version.breaking() {
-        println!("One or more breaking changes");
+    if args.verbose {
+        eprintln!("Next Version\n------------\n");
+        eprintln!(
+            "Conventional commits by type for version: {}",
+            latest_version.name()
+        );
+        eprintln!("  feat:       {}", latest_version.feat_commits());
+        eprintln!("  fix:        {}", latest_version.fix_commits());
+        eprintln!("  docs:       {}", latest_version.docs_commits());
+        eprintln!("  chore:      {}", latest_version.chore_commits());
+        eprintln!("  refactor:   {}", latest_version.refactor_commits());
+        if latest_version.breaking() {
+            eprintln!("One or more breaking changes");
+        } else {
+            eprintln!("No breaking change.");
+        }
+        println!("Next Version: {}", latest_version.next());
     } else {
-        println!("No breaking change.");
+        println!("{}", latest_version.next());
     }
-
-    println!("Next Version: {}", latest_version.next());
     Ok(())
 }
